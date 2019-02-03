f4abd823c2dbbc913cc0fd20dd8f20d03c557c7f
Noor Mohammed Anik <noormohammedanik@gmail.com>
2019-02-03 18:58:54 +0600

    Initial commit

---
+++ app/src/main/java/com/anik/googlemaps/MapsActivity.java
@@ -0,0 +1,100 @@
+package com.anik.googlemaps;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.arsy.maps_library.MapRipple;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    MapRipple mapRipple;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng chittagong = new LatLng(22.330370, 91.832626);
+        mMap.addMarker(new MarkerOptions().position(chittagong).title("Marker in Chittagong"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(chittagong));
+
+        mMap.setMapType(googleMap.MAP_TYPE_NORMAL);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
+        float zoomLevel=14.0f; //up to 21
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(chittagong,zoomLevel));
+
+
+        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        double longitude = location.getLongitude();
+        double latitude = location.getLatitude();
+
+        LatLng myLocation = new LatLng(latitude, longitude);
+
+
+        // mMap is GoogleMap object, latLng is the location on map from which ripple should start
+        mapRipple = new MapRipple(mMap, myLocation, MapsActivity.this);
+        mapRipple.startRippleMapAnimation();      //in onMapReadyCallBack
+
+        // Start Animation again only if it is not running
+        if (!mapRipple.isAnimationRunning()) {
+            mapRipple.startRippleMapAnimation();
+        }
+
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mapRipple.isAnimationRunning()) {
+            mapRipple.stopRippleMapAnimation();
+        }
+    }
+
+}
